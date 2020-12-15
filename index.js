2a89c7e8a41c1d9cea7484afb92aab05f47d4088
Farhan Hasin Chowdhury <mail@farhan.info>
2020-12-15 14:28:21 +0600

    Initial release

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const { Role, Permission } = require('./lib/models');
-
 module.exports = {
-  Role,
-  Permission,
   permissions: require('./lib/permissions'),
-  roleSchema: require('./lib/models/Role').roleSchema,
-  permissionSchema: require('./lib/models/Permission').permissionSchema,
+  roleSchema: require('./lib/schemas/role'),
+  permissionSchema: require('./lib/schemas/permission'),
 };
